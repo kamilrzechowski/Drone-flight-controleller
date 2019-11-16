f983197fce3e9308ac25caccfc5feacab0d584b1
Kamil <49376703+kamilrzechowski@users.noreply.github.com>
2019-11-16 15:29:25 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,23 +1,11 @@
 /**
  ******************************************************************************
  * @file    main.c
- * @author  MCD Application Team
+ * @author  Kamil Rzechowski
  * @version V1.0.0
- * @date    28-October-2011
  * @brief   Main program body
  ******************************************************************************
- * @attention
- *
- * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
- * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
- * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
- * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
- * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
- * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
- *
- * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
- */
-
+ 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "utils.h"
@@ -279,9 +267,3 @@ int main(void) {
 		}
 	}
 }
-
-/**
- * @}
- */
-
-/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
